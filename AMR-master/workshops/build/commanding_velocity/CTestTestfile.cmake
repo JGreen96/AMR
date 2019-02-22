70d9b33528a21213d921f47dab904a8fad6fddc2
JGreen96 <JGreen96@users.noreply.github.com>
2019-02-22 15:59:26 +0000

    week 4 stuff

---
+++ AMR-master/workshops/build/commanding_velocity/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/student/AMR-master/workshops/src/commanding_velocity
+# Build directory: /home/student/AMR-master/workshops/build/commanding_velocity
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
