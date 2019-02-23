7444a79bac2a602305c1292ed86ced27b974cde0
JGreen96 <JGreen96@users.noreply.github.com>
2019-02-23 15:34:07 +0000

    assignment stuff

---
+++ Assignmnet/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/student/Assignmnet/src
+# Build directory: /home/student/Assignmnet/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
